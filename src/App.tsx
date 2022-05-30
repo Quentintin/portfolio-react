5c75c07cd6e9d01028bb44acc2d782c80aa4132b
Quentin DESBOIS <1795716+Quentintin@users.noreply.github.com>
2022-05-30 11:14:17 -0400

    :lipstick: style: Update loading page
    add file

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import ErrorPage from './components/ErrorPage';
+import Spinner from './components/Spinner';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="bg-container">
-      <Suspense fallback="loading">
+      <Suspense fallback={<Spinner />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<ErrorPage />} />
--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="text-center">
-        <h1 className="tracking-tightest font-bold] text-8xl font-black">404</h1>
+        <h1 className="tracking-tightest text-8xl font-black">404</h1>
         <h2
           className="tracking-tightest text-2xl
          font-bold leading-3 text-amber-400">
