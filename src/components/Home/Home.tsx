ec1bd09d663fada33f634fccafc1d43c422bdd1b
Quentin DESBOIS <1795716+Quentintin@users.noreply.github.com>
2022-06-06 18:02:38 -0400

    :sparkles: feat: Add scroll animation with AOS lib

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import Navbar from './navbar/Navbar';
 import Intro from './sections/intro/Intro';
 import Experience from './sections/experience/Experience';
@@ -6,18 +8,55 @@ import Projects from './sections/projects/Projects';
 import Education from './sections/education/Education';
 import Contact from './sections/contact/Contact';
 import Footer from './footer/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Home() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <div id="sectionList" className="container mx-auto grid">
-        <Intro />
-        <Experience />
-        <Skills />
-        <Projects />
-        <Education />
-        <Contact />
+      <div className="grid">
+        <div
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="top-center">
+          <Intro />
+        </div>
+        <div
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="top-center">
+          <Skills />
+        </div>
+        <div
+          data-aos="zoom-in-up"
+          data-aos-duration="800"
+          data-aos-anchor-placement="center-bottom">
+          <Experience />
+        </div>
+        <div
+          data-aos="zoom-in-up"
+          data-aos-duration="800"
+          data-aos-anchor-placement="center-bottom">
+          <Projects />
+        </div>
+        <div
+          data-aos="zoom-in-up"
+          data-aos-duration="800"
+          data-aos-anchor-placement="center-bottom">
+          <Education />
+        </div>
+        <div
+          data-aos="zoom-in-up"
+          data-aos-duration="800"
+          data-aos-anchor-placement="center-bottom">
+          <Contact />
+        </div>
       </div>
       <Footer />
     </>
