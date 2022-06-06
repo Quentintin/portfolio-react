354f714400f8b01bf8a74d355e99013c61c77040
Quentin DESBOIS <1795716+Quentintin@users.noreply.github.com>
2022-06-06 01:21:42 -0400

    :lipstick: style(footer): moon image as footer

--- src/components/Home/footer/Footer.tsx
+++ src/components/Home/footer/Footer.tsx
@@ -1,9 +1,13 @@
 import './Footer.scss';
+import Earth from './../../../assets/images/earth.png';
+import EarthCatoon from './../../../assets/images/earth-cartoon.png';
+import Moon from './../../../assets/images/moon.png';
+import MoonTintin from './../../../assets/images/moon_tintin.png';
 
 export default function Footer() {
   return (
     <footer>
-      <div className="planet"></div>
+      <img src={MoonTintin} alt="Planet" className="max-w-7xl object-cover" />
     </footer>
   );
 }
