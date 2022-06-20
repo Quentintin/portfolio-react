1bc5f903a04236be6d09d36d0468d47c52b1c156
Quentin DESBOIS <1795716+Quentintin@users.noreply.github.com>
2022-06-20 13:41:56 -0400

    :sparkles: feat: Update Contact section

--- src/components/Home/sections/contact/Contact.tsx
+++ src/components/Home/sections/contact/Contact.tsx
@@ -1,60 +1,59 @@
 import { Element } from 'react-scroll';
 import { useTranslation } from 'react-i18next';
+import Card from '../../../common/Card';
+import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { IoLogoYoutube } from 'react-icons/io';
 
 export default function Contact() {
   const { t } = useTranslation();
   return (
-    <Element name="contact" className="container mx-auto mt-12 mb-40 ">
-      <h1 className="my-6 text-right">{t('contact.title')}</h1>
-      <div className="flex flex-wrap sm:flex-nowrap">
-        <form
-          name="contact"
-          className="mt-8 flex w-full flex-col md:ml-auto md:mt-0 md:w-1/2 md:py-8 lg:w-1/3">
-          <h2 className="title-font mb-1 text-3xl font-medium text-white sm:text-4xl">
-            Hire Me
-          </h2>
-          <p className="mb-5 leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum suscipit officia
-            aspernatur veritatis. Asperiores, aliquid?
-          </p>
-          <div className="relative mb-4">
-            <label htmlFor="name" className="text-sm leading-7 text-gray-400">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full rounded border border-gray-700 bg-gray-800 py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900"
-            />
+    <Element name="contact" className="my-4 md:my-6 xl:my-8">
+      <div className="container mx-auto">
+        <Card>
+          <h1>{t('contact.title')}</h1>
+          <div className="my-5 flex flex-wrap items-center justify-evenly">
+            <a
+              href="mailto:contact@quentindesbois.me"
+              className="h-16 w-16 rounded-full bg-teal-500/10 text-teal-500 duration-500 ease-out hover:bg-teal-500/20 lg:h-24 lg:w-24 ">
+              <div className="flex h-full flex-col items-center justify-center">
+                <FaEnvelope className="mb-1 text-xl lg:text-3xl" />
+                <div className="text-centertext-xs font-bold lg:text-sm">
+                  {t('contact.email')}
+                </div>
+              </div>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/quentin-desbois/"
+              target={'_blank'}
+              rel="noreferrer noopener"
+              className="h-16 w-16 rounded-full  bg-blue-500/10 text-blue-500 duration-500 ease-out hover:bg-blue-500/20 lg:h-24 lg:w-24 ">
+              <div className="flex h-full flex-col items-center justify-center">
+                <FaLinkedin className="mb-1  text-xl lg:text-3xl" />
+                <div className="text-center text-xs font-bold lg:text-sm">LinkedIn</div>
+              </div>
+            </a>
+            <a
+              href="https://github.com/quentintin"
+              target={'_blank'}
+              rel="noreferrer noopener"
+              className="h-16 w-16 rounded-full bg-white/10 text-white duration-500 ease-out hover:bg-white/20 lg:h-24 lg:w-24 ">
+              <div className="flex h-full flex-col items-center justify-center">
+                <FaGithub className="mb-1  text-xl lg:text-3xl" />
+                <div className="text-center text-xs font-bold lg:text-sm">Github</div>
+              </div>
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UC0f9enuKE8PPT-9FmqRkiGw"
+              target={'_blank'}
+              rel="noreferrer noopener"
+              className="h-16 w-16 rounded-full bg-red-500/10 text-red-500 duration-500 ease-out hover:bg-red-500/20 lg:h-24 lg:w-24 ">
+              <div className="flex h-full flex-col items-center justify-center">
+                <IoLogoYoutube className="mb-1  text-xl lg:text-3xl" />
+                <div className="text-center text-xs font-bold lg:text-sm">Youtube</div>
+              </div>
+            </a>
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="text-sm leading-7 text-gray-400">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full rounded border border-gray-700 bg-gray-800 py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="message" className="text-sm leading-7 text-gray-400">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="h-32 w-full resize-none rounded border border-gray-700 bg-gray-800 py-1 px-3 text-base leading-6 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900"
-            />
-          </div>
-          <button
-            type="submit"
-            className="rounded border-0 bg-indigo-500 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none">
-            Submit
-          </button>
-        </form>
+        </Card>
       </div>
     </Element>
   );
